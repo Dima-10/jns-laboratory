6218cf5772eb829004607bee5916fc172f14eebe
Dima-10 <dimadragon2x5@gmail.com>
2021-08-17 19:03:12 +0500

    uploading website beginning header

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="Icon-font-7-stroke-PIXEDEN-v-1-2-0/Icon-font-7-stroke-PIXEDEN-v-1.2.0/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
+    <link rel="stylesheet" href="Icon-font-7-stroke-PIXEDEN-v-1-2-0/Icon-font-7-stroke-PIXEDEN-v-1.2.0/pe-icon-7-stroke/css/helper.css">
+    
+    <link rel="stylesheet" href="/style/style.css">
+</head>
+
+
+<body>
+<section class="section-1">
+    <header class="header">
+        <nav class="header__nav">
+            <div class="header__border">
+                <div class="container header__nav-top">
+                
+                    <div class="header__nav-logo"><img src="img/logo.png" alt=""></div>
+                    <div class="header__nav-info">
+                        <div class="header__nav-info-location">
+                            
+                        г. Ташкент,<br>
+                        Учтепинский район,<br>
+                        м-в Алгоритм-31 15А<br>
+                        </div>
+                        <div class="header__nav-info-names">
+                        тел.<br>
+                        факс.<br>
+                        e-mail:<br>
+                        </div>
+                        <div class="header__nav-info-links">
+                        <a href="tel:+99 871 270-06-76">+99 871 270-06-76</a><br>
+                        <a href="tel:+99 871 270-82-02">+99 871 270-82-02</a><br>
+                        <a href="#">uz@jnslabs.com</a><br>
+                        </div>
+                    </div>
+                    <button class="header__nav-btn">Скачать заявку</button>
+                </div>
+            </div>
+            
+                
+                
+           
+            <div class="container header__nav-bottom">
+                <div class="header__nav-bottom-links">
+                    <ul>
+                        <li><a href="" class="header__nav-bottom-link">Испытательная лаборатория</a></li>
+                        <li><a href="" class="header__nav-bottom-link">Оборудование</a></li>
+                        <li><a href="" class="header__nav-bottom-link">Контакты</a></li>
+                    </ul>
+                </div>
+                <div class="header__nav-bottom-social">
+                    <ul class="header__nav-bottom-social-links">
+                        <li><a class="social-link-1 " href=""><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a class="social-link" href=""><i class="fab fa-instagram"></i></a></li>
+                    </ul>
+                    <div class="inf-lang">
+                        <ul class="dropbox-lang d-flex">
+                            
+                            <li><span class="lang-li d-flex align-items-center">ru<i class="fas fa-caret-down"></i></span>
+                                <ul class="dropbox-ul">
+                                    <li class="lang">en</li>
+                                    <li class="lang">uz</li>
+                                </ul>
+
+
+
+                            </li>
+                        </ul>
+
+
+                    </div>
+
+                </div>
+            </div>
+        </nav>     
+        
+        <div class="header__content">
+            <div class="header__content-bg">
+                <div class="header__content-about">
+                    <div class="container">
+                        <div class="header__content-about-item">
+                            <p class="header__content-about-item-number">01</p>
+                            <p class="header__content-about-item-text">Собственное<br> производство</p>
+                        </div>
+                        <div class="header__content-about-item">
+                            <p class="header__content-about-item-number">02</p>
+                            <p class="header__content-about-item-text">Полный<br> комплекс услуг</p>
+                        </div>
+                        <div class="header__content-about-item">
+                            <p class="header__content-about-item-number">03</p>
+                            <p class="header__content-about-item-text">Команда опытных<br> профессионалов</p>
+                        </div>
+                        <div class="header__content-about-item">
+                            <p class="header__content-about-item-number">04</p>
+                            <p class="header__content-about-item-text">Доступные<br> цены</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                
+            
+        </div>
+    </header>
+</section> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="js/script.js"></script>
+    
+</body>
+    
+</html>
---
+++ js/script.js
@@ -0,0 +1,17 @@
+const lang = document.querySelector('.inf-lang .lang-li'),
+    dropboxLang = document.querySelector('.inf-lang ul ul'),
+    dropboxLi = document.querySelector('.inf-lang ul li');
+
+lang.addEventListener('click', function () {
+    dropboxLang.classList.toggle('activeLang')
+
+
+
+})
+/*dropboxLi.addEventListener('click', function () {
+    
+    dropboxLang.classList.remove('activeLang')
+
+})
+*/
+
